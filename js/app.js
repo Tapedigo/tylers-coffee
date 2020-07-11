b3d012a7093c6c85d12fcef46530664343aa6574
Tapedigo <Tapedigo1996@gmail.com>
2020-07-10 20:37:57 -0400

    added some more javascript image carousel still not working

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //Carousel
 
 const carouselSlide = document.querySelector('.carousel');
-const carouselImages = document.querySelectorAll('c-img');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
 
 // Buttons
 const prevBtn = document.querySelector('#prev');
@@ -9,7 +9,39 @@ const nextBtn = document.querySelector('#next');
 
 // Counter
 
-let Counter = 1;
+let counter = 1;
 const size = carouselImages[0].clientWidth;
 
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+// Button Listeners
+
+nextBtn.addEventListener('click',()=>{    
+    
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+});
+
+prevBtn.addEventListener('click',()=>{
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    
+});
+
+carouselSlide.addEventListener('transitionend', ()=>{
+    if (carouselImages[counter].id === 'lastClone') {
+        carouselSlide.style.transform = 'none';  
+        counter = carouselImages.length -2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    carouselSlide.addEventListener('transitionend', ()=>{
+        if (carouselImages[counter].id === 'firstClone') {
+            carouselSlide.style.transform = 'none';  
+            counter = carouselImages.length - counter;
+            carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        }
+    });
+});
