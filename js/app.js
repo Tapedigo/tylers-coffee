f8dd8b71a4f531d4b6d7f2fb1724fbd82512d562
Tapedigo <Tapedigo1996@gmail.com>
2020-07-14 21:45:38 -0400

    added error for forms not working

--- js/app.js
+++ js/app.js
@@ -11,3 +11,20 @@ var swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+const name = document.querySelector('#name');
+const password = document.querySelector('#password');
+const form = document.querySelector('.form');
+const errorElement = document.querySelector('#error');
+
+form.addEventListener('submit' , (e) => {
+  let messages = []
+  if (name.value === '' || name.value == null) {
+    messages.push('Name is required')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+})
