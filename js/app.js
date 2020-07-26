e636c22d450b318d16996bb49564c6f4dceb7a62
Tapedigo <Tapedigo1996@gmail.com>
2020-07-26 12:58:17 -0400

    added hamburger menu not working at present

--- js/app.js
+++ js/app.js
@@ -12,3 +12,14 @@ var swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+// Hamburger Menu
+
+function myFunction() {
+  const x = document.querySelectorAll(".main-nav");
+  if (x.style.display === "block") {
+    x.style.display === "none";
+  } else {
+    x.style.display = "block";
+  }
+}
